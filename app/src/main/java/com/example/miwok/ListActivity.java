69bee3807a6d948e82256f15937e05585934a0f9
Mikayla navarro <MikaylaNavarro99@gmail.com>
2022-10-18 13:01:41 -0500

    done

---
+++ app/src/main/java/com/example/miwok/ListActivity.java
@@ -0,0 +1,59 @@
+package com.example.miwok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("Where are you going?");
+        words.add("What is your name?");
+        words.add("My name is...");
+        words.add("How are you feeling?");
+        words.add("I'm feeling good");
+        words.add("Are you coming?");
+        words.add("Yes, I'm coming");
+        words.add("I'm coming");
+        words.add("Lets go");
+        words.add("Come here");
+
+        LinearLayout<> rootView = (LinearLayout)findViewById(R.id.rootView);
+
+
+
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(itemsAdapter);
+
+
+
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/miwok/Word.java
@@ -0,0 +1,22 @@
+package com.example.miwok;
+
+import java.util.ArrayList;
+
+public class Word  {
+
+private String mDefaultTranslation;
+private String mMiwokTranslation;
+
+private int mImageResourceId);
+
+public Word(String mDefaultTranslation, String miwokTranslation){
+    mDefaultTranslation = defaultTranslation;
+    mMiwokTranslation = miwokTranslation;
+}
+public String getmDefaultTranslation(){ return mDefaultTranslation;}
+
+public String getTranslation(){ return mMiwokTranslation;}
+
+
+
+}
